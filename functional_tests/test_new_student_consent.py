1ac6758682b7eb4947d82fafda482abfed01efa0
sameerbhatnagar <sameer.bhatnagar@gmail.com>
2018-07-16 16:39:53 -0400

    workign test

--- functional_tests/test_new_student_consent.py
+++ functional_tests/test_new_student_consent.py
@@ -1,12 +1,4 @@
-from django.contrib.auth.hashers import make_password
-from django.test import LiveServerTestCase
 from selenium import webdriver
-
-from django_lti_tool_provider.models import LtiUserData
-from django_lti_tool_provider.views import LTIView
-from peerinst.models import User, Teacher
-
-from django.test.utils import override_settings
 
 from peerinst.tests.test_views import QuestionViewTestCase
 
@@ -15,6 +7,7 @@ class NewStudentConsentTest(QuestionViewTestCase):
 
 
     def setUp(self):
+        super(NewStudentConsentTest,self).setUp()        
         self.browser = webdriver.Chrome()
         self.browser.implicitly_wait(10)
 
