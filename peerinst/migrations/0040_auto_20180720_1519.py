959b43b540891397a9d38f8fade9dc2510878ef5
Jonathon Sumner <jonathonresumner@gmail.com>
2018-07-20 11:20:10 -0400

    migrate db

---
+++ peerinst/migrations/0040_auto_20180720_1519.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('peerinst', '0039_auto_20180718_2348'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='studentgroup',
+            name='creation_date',
+            field=models.DateField(auto_now=True, null=True),
+        ),
+    ]
