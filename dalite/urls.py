2fac74def9b830a74595fff7ae3b76d51fc2c889
Jonathon Sumner <jonathonresumner@gmail.com>
2018-07-27 15:58:30 -0400

    language framework activated

--- dalite/urls.py
+++ dalite/urls.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
@@ -11,7 +12,8 @@ import views
 
 admin.site.site_header = admin.site.site_title = _('Dalite NG administration')
 
-urlpatterns = [
+# Apps
+urlpatterns = i18n_patterns(
     url(r'^tos/', include('tos.urls', namespace='tos')),
     url(r'', include('peerinst.urls')),
     url(r'^grappelli/', include('grappelli.urls')),
@@ -22,4 +24,10 @@ urlpatterns = [
     ),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^lti/', include('django_lti_tool_provider.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
+
+# Set language view
+urlpatterns += [url(r'^i18n/', include('django.conf.urls.i18n')),]
+
+# Media
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
