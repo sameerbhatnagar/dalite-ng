079edcb76eb3162e67825f314e91622b8132ea60
sameerbhatnagar <sameer.bhatnagar@gmail.com>
2018-07-27 14:01:14 -0400

    admin logentry

--- peerinst/admin.py
+++ peerinst/admin.py
@@ -5,6 +5,7 @@ import re,json
 from django.core import exceptions
 from django import forms
 from django.contrib import admin
+from django.contrib.admin.models import LogEntry
 from django.utils.translation import ugettext_lazy as _
 from .models import (
     Answer,
@@ -242,3 +243,65 @@ class VerifiedDomainAdmin(admin.ModelAdmin):
 @admin.register(LtiEvent)
 class LtiEventAdmin(admin.ModelAdmin):
     readonly_fields = ["event_type","event_log","timestamp"]
+
+from django.contrib.admin.models import LogEntry, DELETION
+from django.utils.html import escape
+from django.core.urlresolvers import reverse
+
+# https://djangosnippets.org/snippets/2484/
+class LogEntryAdmin(admin.ModelAdmin):
+
+    date_hierarchy = 'action_time'
+
+    readonly_fields = LogEntry._meta.get_all_field_names()
+
+    list_filter = [
+        'user',
+        'content_type',
+        'action_flag'
+    ]
+
+    search_fields = [
+        'object_repr',
+        'change_message'
+    ]
+
+
+    list_display = [
+        'action_time',
+        'user',
+        'content_type',
+        'object_link',
+        'action_flag',
+        'change_message',
+    ]
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_superuser and request.method != 'POST'
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def object_link(self, obj):
+        if obj.action_flag == DELETION:
+            link = escape(obj.object_repr)
+        else:
+            ct = obj.content_type
+            link = u'<a href="%s">%s</a>' % (
+                reverse('admin:%s_%s_change' % (ct.app_label, ct.model), args=[obj.object_id]),
+                escape(obj.object_repr),
+            )
+        return link
+    object_link.allow_tags = True
+    object_link.admin_order_field = 'object_repr'
+    object_link.short_description = u'object'
+    
+    def queryset(self, request):
+        return super(LogEntryAdmin, self).queryset(request) \
+            .prefetch_related('content_type')
+
+
+admin.site.register(LogEntry, LogEntryAdmin)
