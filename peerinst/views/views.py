858122f55cae9f678f5ac8dd52a850036b3018e9
Jonathon Sumner <jonathonresumner@gmail.com>
2018-09-18 12:34:29 -0400

    only return questions with answer choices, add progress indicator, disable search bar while waiting for ajax response

--- peerinst/views/views.py
+++ peerinst/views/views.py
@@ -2438,7 +2438,7 @@ def question_search(request):
 
             query_term = query_term.exclude(id__in=q_qs).distinct()
 
-            query_term = [q for q in query_term if q not in query_all]
+            query_term = [q for q in query_term if q not in query_all and q.answerchoice_set.count() > 0]
 
             query_meta[term] = query_term
 
