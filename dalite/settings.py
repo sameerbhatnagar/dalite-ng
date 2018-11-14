dc90b4c3c8c9e2910e13174f37b483ec28ab698a
Antoine Lefebvre-Brossard <antoinelb@protonmail.com>
2018-11-14 13:05:56 -0500

    Put back settings to what it was

--- dalite/settings.py
+++ dalite/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 # Application definition
 
