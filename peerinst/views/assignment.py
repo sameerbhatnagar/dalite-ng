15a473ff11224631561b42a9ffbba55e505198f9
Antoine Lefebvre-Brossard <antoinelb@protonmail.com>
2018-11-26 09:43:17 -0500

    Corrected imports

--- peerinst/views/assignment.py
+++ peerinst/views/assignment.py
@@ -2,24 +2,17 @@
 from __future__ import unicode_literals
 
 from django.contrib.auth.decorators import login_required
-from django.http import (
-    HttpResponse,
-    HttpResponseBadRequest,
-    HttpResponseForbidden,
-    HttpResponseRedirect,
-    HttpResponseServerError,
-    JsonResponse,
-)
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseBadRequest
 from django.template.response import TemplateResponse
 from django.utils.translation import ugettext_lazy as _
 from django.views.decorators.http import require_http_methods
 import json
-import pytz
 
 from peerinst.models import Assignment, Question
 
 from .decorators import teacher_required
+
+from django.core.exceptions import PermissionDenied
 
 
 @login_required
@@ -51,17 +44,9 @@ def update_assignment_question_list(req):
         return HttpResponseBadRequest(resp.render())
 
     # Check object permissions (to be refactored using mixin)
-    if (
-        req.user in assignment.owner.all()
-        or req.user.is_staff
-    ):
+    if req.user in assignment.owner.all() or req.user.is_staff:
         # Check for student answers
-        if (
-            assignment
-            .answer_set.exclude(user_token__exact="")
-            .count()
-            > 0
-        ):
+        if assignment.answer_set.exclude(user_token__exact="").count() > 0:
             raise PermissionDenied
     else:
         raise PermissionDenied
@@ -82,4 +67,4 @@ def update_assignment_question_list(req):
         assignment.questions.add(question)
     assignment.save()
 
-    return HttpResponse('Success')
+    return HttpResponse("Success")
