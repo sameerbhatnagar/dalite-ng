a49577d015de6e8a91e03b24165008c224511c4a
jsumnerPhD <jsumner@dawsoncollege.qc.ca>
2018-12-09 20:36:18 -0500

    Update ajax function for getting csrftoken and register notifications in the admin

--- peerinst/static/peerinst/js/_ajax/utils.js
+++ peerinst/static/peerinst/js/_ajax/utils.js
@@ -1,22 +1,20 @@
 "use strict";
 
 export function getCsrfToken() {
-  let name = "csrftoken";
-  if (document.cookie && document.cookie !== "") {
-    return document.cookie
-      .split(";")
-      .filter(
-        c =>
-          c
-            .replace(/^\s+/, "")
-            .replace(/\s*$/, "")
-            .substring(0, name.length + 1) ===
-          name + "=",
-      )
-      .map(c => decodeURIComponent(c.substring(name.length + 1)))[0];
-  } else {
-    return null;
+  const name = "csrftoken";
+  let cookieValue = null;
+  if (document.cookie && document.cookie != "") {
+    const cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = jQuery.trim(cookies[i]);
+      // Does this cookie string begin with the name we want?
+      if (cookie.substring(0, name.length + 1) == name + "=") {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
   }
+  return cookieValue;
 }
 
 export function buildReq(data, method) {
