4e6c367813250fe2004666896303f203eda74a1f
Antoine Lefebvre-Brossard <antoinelb@protonmail.com>
2018-12-10 09:37:04 -0500

    Fixed ajax call

--- peerinst/static/peerinst/js/_ajax/utils.js
+++ peerinst/static/peerinst/js/_ajax/utils.js
@@ -2,22 +2,27 @@
 
 export function getCsrfToken() {
   const name = "csrftoken";
-  let cookieValue = null;
-  if (document.cookie && document.cookie != "") {
-    const cookies = document.cookie.split(";");
-    for (let i = 0; i < cookies.length; i++) {
-      const cookie = jQuery.trim(cookies[i]);
-      // Does this cookie string begin with the name we want?
-      if (cookie.substring(0, name.length + 1) == name + "=") {
-        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-        break;
-      }
-    }
+  if (document.cookie && document.cookie !== "") {
+    return document.cookie
+      .split(";")
+      .filter(
+        c =>
+          c
+            .replace(/^\s+/, "")
+            .replace(/\s*$/, "")
+            .substring(0, name.length + 1) ===
+          name + "=",
+      )
+      .map(c =>
+        decodeURIComponent(c.substring(name.length + 1)).replace(/^=/, ""),
+      )[0];
+  } else {
+    return null;
   }
-  return cookieValue;
 }
 
 export function buildReq(data, method) {
+  console.log(getCsrfToken());
   return {
     method: method.toUpperCase(),
     body: JSON.stringify(data),
