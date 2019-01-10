0614b1f722e414d364ff95c463830b0ead699969
Antoine Lefebvre-Brossard <antoinelb@protonmail.com>
2019-01-10 17:42:26 -0500

    Removed hover_text from student notification

--- peerinst/migrations/0059_remove_studentnotification_hover_text.py
+++ peerinst/migrations/0059_remove_studentnotification_hover_text.py
@@ -6,13 +6,10 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('peerinst', '0058_merge'),
-    ]
+    dependencies = [("peerinst", "0058_merge")]
 
     operations = [
         migrations.RemoveField(
-            model_name='studentnotification',
-            name='hover_text',
-        ),
+            model_name="studentnotification", name="hover_text"
+        )
     ]
