c42f42e57153e2b6afef4208cb3dcc15f175a86b
Antoine Lefebvre-Brossard <antoinelb@protonmail.com>
2019-01-11 13:40:50 -0500

    Revert "Upgraded django to version 1.11"
    This reverts commit 1703fcecd09e6fb59e154a81815634c657607649.

--- peerinst/__init__.py
+++ peerinst/__init__.py
@@ -1,1 +1,1 @@
-default_app_config = "peerinst.apps.PeerinstConfig"
+default_app_config = 'peerinst.apps.PeerInstConfig'
--- tos/app.py
+++ tos/app.py
@@ -1,13 +1,12 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django import apps
 from django.utils.translation import ugettext_lazy as _
 
 
-class TosConfig(apps.AppConfig):
+class ConsentConfig(apps.AppConfig):
     name = "tos"
     verbose_name = _("Dalite user consents")
 
     def ready(self):
-        import tos.signals  # noqa
+        import tos.signals
