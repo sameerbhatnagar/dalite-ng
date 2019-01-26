34d7bda5986498ca6a1455b0e9a801bba1325299
sambhatnagar <sameer.bhatnagar@gmail.com>
2019-01-25 21:32:23 -0500

    group gradebook updated for results by assignment

--- peerinst/gradebook.py
+++ peerinst/gradebook.py
@@ -2,7 +2,11 @@ import csv
 
 from django.db.models.functions import Lower
 
-from .models import Answer, StudentGroupMembership
+from .models import (
+    Answer,
+    StudentGroupMembership,
+    StudentAssignment,
+    )
 
 
 class Echo:
@@ -19,16 +23,85 @@ class Echo:
 
 
 def group_gradebook(group):
+    """
+    Parameters
+    ----------
+    StudentGroup Hash
+
+    Returns:
+    --------
+    csv writer object with aggregate results for each student
+    across all assignments
+    """
+
     memberships = StudentGroupMembership.objects.filter(group=group).order_by(
         Lower("student__student__email")
     )
 
-    # TODO handle not distributed assignments
     assignments = group.studentgroupassignment_set.filter(
-    distribution_date__isnull=False
+        distribution_date__isnull=False
     ).order_by(
         "distribution_date"
     )
+
+    pseudo_buffer = Echo()
+    writer = csv.writer(pseudo_buffer)
+
+    header = []
+    if group.student_id_needed:
+        header.append("Student ID")
+    header.append("Student Email")
+    for assignment in assignments:
+        header.extend([
+            "{} - {}".format(
+            quantity,assignment.assignment.identifier
+            ) for quantity in ['n_correct','n_completed']
+        ])
+    yield writer.writerow(header)
+
+    for membership in memberships:
+        row = []
+
+        if group.student_id_needed:
+            row.append(membership.student_school_id)
+
+        row.append(membership.student.student.email)
+
+        for assignment in assignments:
+            try:
+                row.append(
+                    assignment.studentassignment_set.get(
+                            student=membership.student
+                        ).results['n_correct']
+                    )
+                row.append(
+                    assignment.studentassignment_set.get(
+                            student=membership.student
+                        ).results['n_completed']
+                    )
+            except StudentAssignment.DoesNotExist:
+                row.append("-")
+                row.append("-")
+        yield writer.writerow(row)
+
+def groupassignment_gradebook(group,assignment):
+    """
+    Parameters
+    ----------
+    StudentGroup Hash
+    StudentGroupAssignment hash
+
+    Returns:
+    -------
+    csv writer object with results for each student in this assignment
+    for each question
+
+    """
+
+    memberships = StudentGroupMembership.objects.filter(group=group).order_by(
+        Lower("student__student__email")
+    )
+
 
     answers = Answer.objects.filter(
         assignment__in=assignments.values_list("assignment", flat=True),
