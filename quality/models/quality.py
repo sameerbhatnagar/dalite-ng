fe41833357c0d953976f3bc99e647791e43a4e1e
Antoine Lefebvre-Brossard <antoinelb@protonmail.com>
2019-02-26 17:06:04 -0500

    Put quality evaluation in `Quality`

--- quality/models/quality.py
+++ quality/models/quality.py
@@ -5,8 +5,6 @@ import logging
 
 from django.db import models
 
-from peerinst.models import Answer
-
 from .criterion.criterion_list import criterions
 
 logger = logging.getLogger("quality")
@@ -21,7 +19,13 @@ class UsesCriterion(models.Model):
     def save(self, *args, **kwargs):
         """
         Saves the new criterion making sure only one exists for each criterion
-        name.
+        `name` and the given `name` and version correspond to a criterion.
+
+        Raises
+        ------
+        ValueError
+            If either the `name` or `version` correspond to a non-existing
+            criterion
         """
         try:
             criterion = criterions[self.name]
@@ -30,7 +34,7 @@ class UsesCriterion(models.Model):
             raise ValueError(
                 "The used criterion has an invalid name or version."
             )
-        if self.version >= criterion.objects.all().count():
+        if self.version >= criterion.objects.count():
             logger.error(
                 "The criterion %s doesn't have a version %d.",
                 self.name,
@@ -46,27 +50,10 @@ class UsesCriterion(models.Model):
 class Quality(models.Model):
     criterions = models.ManyToManyField(UsesCriterion)
 
-
-class AnswerQuality(models.Model):
-    quality_model = models.ForeignKey(Quality)
-    answer = models.ForeignKey(Answer)
-
-    @property
-    def quality(self):
+    def evaluate(self, answer, *args, **kwargs):
         """
-        Quality as a weigthed mean of the different criterions.
-
-        Returns
-        -------
-        float
-            Quality
-        """
-        return self.broken_down_quality[0]
-
-    @property
-    def broken_down_quality(self):
-        """
-        Quality as a tuple of the quality and the different criterion results.
+        Returns the quality as a tuple of the quality and the different
+        criterion results.
 
         Returns
         -------
@@ -81,14 +68,29 @@ class AnswerQuality(models.Model):
                     quality: float
                 }]
         """
+        criterions_ = [
+            {
+                "criterion": (
+                    criterions[c.name].objects.get(
+                        version=criterions[c.name].objects.count() - 1
+                    )
+                    if c.use_latest
+                    else criterions[c.name].objects.get(version=c.version)
+                ),
+                "weight": c.weight,
+            }
+            for c in self.criterions
+        ]
         qualities = [
             {
-                "name": criterion.criterion.name,
-                "version": criterion.criterion.version,
-                "weight": criterion.weight,
-                "quality": criterion.criterion.evaluate(self.answer),
+                "name": c["criterion"].name,
+                "version": c["criterion"].version,
+                "weight": c["weight"],
+                "quality": c["criterion"].evaluate(
+                    self.answer, *args, **kwargs
+                ),
             }
-            for criterion in self.quality_model.usescriterion_set.all()
+            for c in criterions_
         ]
         quality = sum(q["quality"] * q["weight"] for q in qualities) / sum(
             q["weight"] for q in qualities
