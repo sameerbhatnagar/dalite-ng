d671e00e6614cd5b37eab877bb37ef592f03645e
sambhatnagar <sameer.bhatnagar@gmail.com>
2019-03-12 00:09:57 -0400

    error catch for load shown rationale management command for Rationale Only

--- peerinst/management/commands/load_shown_rationales_from_ltievent_logs.py
+++ peerinst/management/commands/load_shown_rationales_from_ltievent_logs.py
@@ -10,13 +10,32 @@ LOGGER = logging.getLogger("peerinst")
 
 
 class Command(BaseCommand):
-    help = "make ShownRationale objects from LtiEvents"
+    help = """
+    make ShownRationale objects from LtiEvents.ime t
+    Output should be directed to a log file
+    Since this function takes a long time on production db,
+    do one month at a time
+    """
+
+    def add_arguments(self, parser):
+        parser.add_argument("--month", type=int)
+        parser.add_argument("--year", type=int)
 
     def handle(self, *args, **options):
 
-        event_logs = LtiEvent.objects.all().values_list("event_log", flat=True)
-        LOGGER.info("start loading shown rationales")
-        LOGGER.info(str(datetime.datetime.now()))
+        min_date = datetime.datetime(
+            day=1, month=options["month"], year=options["year"]
+        )
+        max_date = datetime.datetime(
+            day=28, month=options["month"], year=options["year"]
+        )
+
+        event_logs = LtiEvent.objects.filter(
+            timestamp__gte=min_date, timestamp__lte=max_date
+        ).values_list("event_log", flat=True)
+        print(len(event_logs))
+        print("start loading shown rationales")
+        print(str(datetime.datetime.now()))
         for e in event_logs:
             e_json = json.loads(e)
             if e_json["event_type"] == "save_problem_success":
@@ -29,28 +48,31 @@ class Command(BaseCommand):
                             assignment_id=e_json["event"]["assignment_id"],
                         )
                     except Answer.MultipleObjectsReturned:
-                        LOGGER.info(
+                        print(
                             "Multiple : ",
                             e_json["username"],
                             e_json["event"]["question_id"],
                             e_json["event"]["assignment_id"],
                         )
-
-                    for r in e_json["event"]["rationales"]:
-                        obj, created = ShownRationale.objects.get_or_create(
-                            shown_answer=Answer.objects.get(pk=r["id"]),
-                            shown_for_answer=shown_for_answer,
-                        )
-                        if created:
-                            LOGGER.info(obj.shown_for_answer, obj.shown_answer)
+                    try:
+                        for r in e_json["event"]["rationales"]:
+                            obj, created = ShownRationale.objects.get_or_create(  # noqa
+                                shown_answer=Answer.objects.get(pk=r["id"]),
+                                shown_for_answer=shown_for_answer,
+                            )
+                            if created:
+                                print(obj.shown_for_answer, obj.shown_answer)
+                    except KeyError:
+                        print("No Rationales")
+                        print(e_json)
 
                 except Answer.DoesNotExist:
-                    LOGGER.info(
+                    print(
                         "Not found : ",
                         e_json["username"],
                         e_json["event"]["question_id"],
                         e_json["event"]["assignment_id"],
                     )
 
-        LOGGER.info("completed loading shown rationales")
-        LOGGER.info(str(datetime.datetime.now()))
+        print("completed loading shown rationales")
+        print(str(datetime.datetime.now()))
