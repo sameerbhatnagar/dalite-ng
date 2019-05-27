26ccb23d16131517de7ccdd2b879dacc27eb56fa
Antoine Lefebvre-Brossard <antoinelb@protonmail.com>
2019-05-27 12:56:14 -0400

    Added cache for quality

---
+++ quality/tests/models/quality/test_quality_cache.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from peerinst.tests.fixtures import *  # noqa
+from quality.models import QualityCache
+from quality.tests.fixtures import *  # noqa
+
+
+def test_cache(global_validation_quality, answers):
+    answer = answers[0]
+    answer.rationale = "test"
+    answer.save()
+
+    quality = 1
+    qualities = {"name": "test", "quality": 1, "threshold": 1}
+
+    n = QualityCache.objects.count()
+    QualityCache.cache(global_validation_quality, answer, quality, qualities)
+    assert QualityCache.objects.count() == n + 1
+
+
+def test_get(global_validation_quality, answers):
+    answer = answers[0]
+    answer.rationale = "test"
+    answer.save()
+
+    quality = 1
+    qualities = {"name": "test", "quality": 1, "threshold": 1}
+
+    n = QualityCache.objects.count()
+    QualityCache.cache(global_validation_quality, answer, quality, qualities)
+    assert QualityCache.objects.count() == n + 1
+
+    quality_, qualities_ = QualityCache.get(global_validation_quality, answer)
+    assert quality_ == quality
+    assert qualities_ == qualities
