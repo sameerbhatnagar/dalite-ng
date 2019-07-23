71c09c890bd17b279dd45c5b2106eb19617f2c67
Antoine Lefebvre-Brossard <antoinelb@protonmail.com>
2019-07-23 08:54:00 -0400

    Added option to only return HttpResponse

--- dalite/views/errors.py
+++ dalite/views/errors.py
@@ -3,12 +3,12 @@ from __future__ import unicode_literals
 
 import logging
 
-from django.template.response import TemplateResponse
+from django.template.response import HttpResponse, TemplateResponse
 
 logger = logging.getLogger("dalite")
 
 
-def response_400(req, msg="", logger_msg="", log=None):
+def response_400(req, msg="", logger_msg="", log=None, use_template=True):
     """
     Returns a 400 error with the passed in msg, logging at the same time.
 
@@ -23,6 +23,16 @@ def response_400(req, msg="", logger_msg="", log=None):
     log : Optional[Callable[[], None]] (default : None)
         Function to use to log. If None, will log a warning with "dalite"
         logger
+    use_template : bool (default : True)
+        Return a template response instead of a regular HttpResponse
+
+    Returns
+    -------
+    Either
+        TemplateResponse
+            Template with the `msg` added if `template_reponse` is True
+        HttpResponse
+            HttpResponse with the `msg` as the text
     """
     if not logger_msg:
         logger_msg = "400 error for user {} on path {}.".format(
@@ -33,12 +43,15 @@ def response_400(req, msg="", logger_msg="", log=None):
 
     log(logger_msg)
 
-    return TemplateResponse(
-        req, "400.html", context={"message": msg}, status=400
-    )
+    if use_template:
+        return TemplateResponse(
+            req, "400.html", context={"message": msg}, status=400
+        )
+    else:
+        return HttpResponse(msg, status=400)
 
 
-def response_403(req, msg="", logger_msg="", log=None):
+def response_403(req, msg="", logger_msg="", log=None, use_template=True):
     """
     Returns a 403 error with the passed in msg, logging at the same time.
 
@@ -53,6 +66,16 @@ def response_403(req, msg="", logger_msg="", log=None):
     log : Optional[Callable[[], None]] (default : None)
         Function to use to log. If None, will log a warning with "dalite"
         logger
+    use_template : bool (default : True)
+        Return a template response instead of a regular HttpResponse
+
+    Returns
+    -------
+    Either
+        TemplateResponse
+            Template with the `msg` added if `template_reponse` is True
+        HttpResponse
+            HttpResponse with the `msg` as the text
     """
     if not logger_msg:
         logger_msg = "403 error for user {} on path {}.".format(
@@ -63,12 +86,15 @@ def response_403(req, msg="", logger_msg="", log=None):
 
     log(logger_msg)
 
-    return TemplateResponse(
-        req, "403.html", context={"message": msg}, status=403
-    )
+    if use_template:
+        return TemplateResponse(
+            req, "403.html", context={"message": msg}, status=403
+        )
+    else:
+        return HttpResponse(msg, status=400)
 
 
-def response_404(req, msg="", logger_msg="", log=None):
+def response_404(req, msg="", logger_msg="", log=None, use_template=True):
     """
     Returns a 404 error with the passed in msg, logging at the same time.
 
@@ -83,6 +109,16 @@ def response_404(req, msg="", logger_msg="", log=None):
     log : Optional[Callable[[], None]] (default : None)
         Function to use to log. If None, will log a warning with "dalite"
         logger
+    use_template : bool (default : True)
+        Return a template response instead of a regular HttpResponse
+
+    Returns
+    -------
+    Either
+        TemplateResponse
+            Template with the `msg` added if `template_reponse` is True
+        HttpResponse
+            HttpResponse with the `msg` as the text
     """
     if not logger_msg:
         logger_msg = "404 error for user {} on path {}.".format(
@@ -93,12 +129,15 @@ def response_404(req, msg="", logger_msg="", log=None):
 
     log(logger_msg)
 
-    return TemplateResponse(
-        req, "404.html", context={"message": msg}, status=404
-    ).render()
+    if use_template:
+        return TemplateResponse(
+            req, "404.html", context={"message": msg}, status=404
+        ).render()
+    else:
+        return HttpResponse(msg, status=400)
 
 
-def response_500(req, msg="", logger_msg="", log=None):
+def response_500(req, msg="", logger_msg="", log=None, use_template=True):
     """
     Returns a 500 error with the passed in msg, logging at the same time.
 
@@ -113,6 +152,16 @@ def response_500(req, msg="", logger_msg="", log=None):
     log : Optional[Callable[[], None]] (default : None)
         Function to use to log. If None, will log a warning with "dalite"
         logger
+    use_template : bool (default : True)
+        Return a template response instead of a regular HttpResponse
+
+    Returns
+    -------
+    Either
+        TemplateResponse
+            Template with the `msg` added if `template_reponse` is True
+        HttpResponse
+            HttpResponse with the `msg` as the text
     """
     if not logger_msg:
         logger_msg = "500 error for user {} on path {}.".format(
@@ -123,6 +172,9 @@ def response_500(req, msg="", logger_msg="", log=None):
 
     log(logger_msg)
 
-    return TemplateResponse(
-        req, "500.html", context={"message": msg}, status=500
-    )
+    if use_template:
+        return TemplateResponse(
+            req, "500.html", context={"message": msg}, status=500
+        )
+    else:
+        return HttpResponse(msg, status=400)
