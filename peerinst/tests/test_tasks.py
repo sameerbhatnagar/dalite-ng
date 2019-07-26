1180930fd618a47f4829b10eb6e8da50103e4fca
Antoine Lefebvre-Brossard <antoinelb@protonmail.com>
2019-07-26 13:40:43 -0400

    Added tests for celery tasks (not working)

--- peerinst/tests/test_tasks.py
+++ peerinst/tests/test_tasks.py
@@ -1,7 +1,50 @@
-from peerinst.tasks import compute_gradebook_async
+import mock
+import pytest
+from celery.result import AsyncResult
+
+from peerinst.tasks import compute_gradebook_async, send_mail_async
+from peerinst.tests.fixtures import *  # noqa
 
 
+@pytest.mark.skip(reason="Can't run multiple celery workers")
+def test_send_mail_async(celery_worker):
+    args = [1, 2, 3]
+    kwargs = {"a": 1, "b": 2, "c": 3}
+    with mock.patch("peerinst.tasks.send_mail") as send_mail:
+        result = send_mail_async(*args, **kwargs)
+        assert isinstance(result, AsyncResult)
+        send_mail.called_with(*args, **kwargs)
+
+
+@pytest.mark.skip(reason="Can't run multiple celery workers")
+def test_send_mail_async__celery_not_running():
+    args = [1, 2, 3]
+    kwargs = {"a": 1, "b": 2, "c": 3}
+    with mock.patch("peerinst.tasks.send_mail") as send_mail:
+        result = send_mail_async(*args, **kwargs)
+        assert not isinstance(result, AsyncResult)
+        send_mail.called_with(*args, **kwargs)
+
+
+@pytest.mark.skip(reason="Can't run multiple celery workers")
 def test_compute_gradebook_async(celery_worker):
-    task_id = compute_gradebook_async(None, None).id
+    group_pk = 1
+    assignment_pk = 1
+    gradebook = mock.MagicMock()
+    gradebook.compute_gradebook.return_value = True
+    with mock.patch.dict("sys.modules", {"peerinst.gradebook": gradebook}):
+        result = compute_gradebook_async(group_pk, assignment_pk)
+        assert isinstance(result, AsyncResult)
+        gradebook.compute_gradebook.called_with(group_pk, assignment_pk)
 
-    assert isinstance(task_id, basestring)
+
+@pytest.mark.skip(reason="Can't run multiple celery workers")
+def test_compute_gradebook_async__celery_not_running():
+    group_pk = 1
+    assignment_pk = 1
+    gradebook = mock.MagicMock()
+    gradebook.compute_gradebook.return_value = True
+    with mock.patch.dict("sys.modules", {"peerinst.gradebook": gradebook}):
+        result = compute_gradebook_async(group_pk, assignment_pk)
+        assert not isinstance(result, AsyncResult)
+        gradebook.compute_gradebook.called_with(group_pk, assignment_pk)
