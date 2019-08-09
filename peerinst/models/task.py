e1f5898ee9ca05803d868b250e51a64b1aa4d8bd
Antoine Lefebvre-Brossard <antoinelb@protonmail.com>
2019-08-09 09:55:53 -0400

    Fixed typo

--- peerinst/models/task.py
+++ peerinst/models/task.py
@@ -9,5 +9,5 @@ from .teacher import Teacher
 class RunningTask(models.Model):
     id = models.CharField(max_length=50, primary_key=True)
     description = models.TextField()
-    teacher = models.ForeignKey(Teacher, related_name="runnning_tasks")
+    teacher = models.ForeignKey(Teacher, related_name="running_tasks")
     datetime = models.DateTimeField(auto_now_add=True)
