d79aab430c2f424b2063aa6a9d85d8b9304dee68
Antoine Lefebvre-Brossard <antoinelb@protonmail.com>
2019-08-13 10:39:02 -0400

    Moved models/message.py file

--- peerinst/models/__init__.py
+++ peerinst/models/__init__.py
@@ -5,7 +5,7 @@ from .assignment import *  # noqa
 from .blink import *  # noqa
 from .collection import *  # noqa
 from .lti import *  # noqa
-from .messages import Message, MessageType, SaltiseMember
+from .message import Message, MessageType, SaltiseMember
 from .question import *  # noqa
 from .search import *  # noqa
 from .student import *  # noqa
