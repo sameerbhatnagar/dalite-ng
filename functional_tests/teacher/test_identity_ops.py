e7d910a75b692fb2ec8da1a2f17ce4dc4e99cccd
jsumnerPhD <jonathon.r.e.sumner@gmail.com>
2019-08-18 20:14:01 -0400

    Fix logic with next GET parameter for email settings view.  Small update to UI.  Functional test still needed to test effect of changing notification settings

--- functional_tests/teacher/test_identity_ops.py
+++ functional_tests/teacher/test_identity_ops.py
@@ -4,11 +4,15 @@ from functional_tests.fixtures import *  # noqa
 from .utils import go_to_account, login, logout
 
 
-def test_change_password(browser, assert_, teacher):
+def start(browser, teacher):
     login(browser, teacher)
     go_to_account(browser)
     browser.find_element_by_id("identity-section").click()
-    browser.find_element_by_id("edit-user-btn").click()
+
+
+def test_change_password(browser, assert_, teacher):
+    start(browser, teacher)
+    browser.find_element_by_id("email-modify-btn").click()
 
     # Check content
     assert (
@@ -59,5 +63,39 @@ def test_change_password(browser, assert_, teacher):
     assert browser.current_url.endswith("browse/")
 
 
-def test_email_settings():
-    pass
+def test_email_address_change(browser, assert_, teacher):
+    new_email_address = "new_email_address@test.com"
+
+    start(browser, teacher)
+    browser.find_element_by_id("email-modify-btn").click()
+    assert "Email Settings" in browser.find_element_by_tag_name("h1").text
+
+    browser.find_element_by_id("id_email").send_keys(new_email_address)
+    browser.find_element_by_id("submit-email-change-btn").click()
+    assert "My Account" in browser.find_element_by_tag_name("h1").text
+
+    browser.find_element_by_id("identity-section").click()
+    browser.wait_for(
+        lambda: assert_(
+            new_email_address
+            in browser.find_element_by_id("edit-user-btn").text
+        )
+    )
+
+
+def test_email_notification_change(browser, assert_, teacher):
+    start(browser, teacher)
+    browser.find_element_by_id("email-modify-btn").click()
+    assert "Email Settings" in browser.find_element_by_tag_name("h1").text
+
+    assert not browser.find_element_by_id(
+        "submit-notification-change-btn"
+    ).is_enabled()
+
+    browser.find_element_by_id("btn-toggle-all").click()
+    browser.find_element_by_id("submit-notification-change-btn").click()
+    assert "My Account" in browser.find_element_by_tag_name("h1").text
+
+    # Send an email... outbox should be empty
+
+    # Reset password... outbox should have 1 message
