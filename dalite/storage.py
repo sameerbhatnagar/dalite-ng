d4b1e1b7a4e69779ac4fc9f432d77d81c763a7a0
sambhatnagar <sameer.bhatnagar@gmail.com>
2019-08-29 11:34:06 -0400

    fix import of boto3 in storages again

--- dalite/storage.py
+++ dalite/storage.py
@@ -1,8 +1,8 @@
 from django.core.files.storage import get_storage_class
-from storages.backends.s3boto import S3BotoStorage
+from storages.backends.s3boto3 import S3Boto3Storage
 
 
-class CachedS3BotoStorage(S3BotoStorage):
+class CachedS3BotoStorage(S3Boto3Storage):
     """
     S3 storage backend that saves the files locally, too.
     """
