ebf7d94824392db84269e542c1b7939d47aec668
sambhatnagar <sameer.bhatnagar@gmail.com>
2019-09-01 21:14:15 -0400

    better choose rationale logic

--- peerinst/rationale_annotation.py
+++ peerinst/rationale_annotation.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from operator import itemgetter
 
-from django.db.models import Count, F, Q
+from django.db.models import Count, Q
 
 from quality.models import Quality
 
@@ -108,75 +108,118 @@ def choose_rationales_no_quality(teacher, n=5):
 
     # if teacher has active students, sample from their most recent student
     # answers
-    current_assignments = teacher.current_groups.values_list(
-        "studentgroupassignment__assignment__identifier", flat=True
-    )
     current_students = student_list_from_student_groups(
-        teacher.current_groups.all().values_list("pk", flat=True)
+        group_list=teacher.current_groups.all()
     )
-    _answers = Answer.objects.filter(
-        user_token__in=current_students, assignment_id__in=current_assignments
-    ).order_by("datetime_second")
 
-    print(_answers.count())
-    print(_answers)
+    # print(len(current_students)," current students")
 
-    # if not, sample from answers of previously active students
+    # if no current_students,or current students have no answers yet,
+    # get previous students
+    if (
+        len(current_students) == 0
+        or Answer.objects.filter(user_token__in=current_students).count() < n
+    ):
+        current_students.extend(
+            student_list_from_student_groups(
+                group_list=teacher.studentgroup_set.all()
+            )
+        )
+    # print(len(current_students)," current students after extend into past")
 
-    # if not, sample from answers to favourite_questions
+    current_assignments = list(
+        teacher.current_groups.values_list(
+            "studentgroupassignment__assignment__identifier", flat=True
+        )
+    )
 
-    # if still nothing, sample from most popular answers in teacher disciplines
+    # print(len(current_assignments)," current assignments")
 
-    # if no teacher discipline, get most popular answers amongst students
+    # if no current_assignments,or current assignments have no answers yet,
+    # get previous assignments
+
+    if (
+        len(current_assignments) == 0
+        or Answer.objects.filter(user_token__in=current_assignments).count()
+        < n
+    ):
+        current_assignments.extend(teacher.assignments.all())
+
+    # print(len(current_assignments),"
+    # current assignments after extend into past")
+
+    if len(current_assignments) > 0 and len(current_students) > n:
+        convincing_rationales = (
+            Answer.objects.filter(
+                user_token__in=current_students,
+                assignment_id__in=current_assignments,
+            )
+            .values("chosen_rationale")
+            .annotate(times_chosen=Count("chosen_rationale"))
+            .order_by("-times_chosen")
+            .values_list("chosen_rationale", flat=True)
+        )
+    elif teacher.favourite_questions.all().count() > 0:
+        # teacher new to dalite will have no students or assignments
+        # first try if they have any favorite questions
+        convincing_rationales = (
+            Answer.objects.filter(
+                question_id__in=teacher.favourite_questions.all(),
+                second_answer_choice__isnull=False,
+            )
+            .values("chosen_rationale")
+            .annotate(times_chosen=Count("chosen_rationale"))
+            .order_by("-times_chosen")
+            .values_list("chosen_rationale", flat=True)
+        )
+    elif teacher.disciplines.all().count() > 0:
+        # if no favourite_questions, try based on disciplines
+        convincing_rationales = (
+            Answer.objects.filter(
+                question_id__in=teacher.disciplines.all().values_list(
+                    "question", flat=True
+                ),
+                second_answer_choice__isnull=False,
+            )
+            .values("chosen_rationale")
+            .annotate(times_chosen=Count("chosen_rationale"))
+            .order_by("-times_chosen")
+            .values_list("chosen_rationale", flat=True)
+        )
+
+    else:
+        # otherwise send nothing
+        convincing_rationales = Answer.objects.none()
+
+    # print(convincing_rationales.count(), " convincing_rationales")
+    # print(convincing_rationales)
 
     # remove answers that have been scored twice already, or that
     # have been scored by the current users
 
-    q_list = list(teacher.favourite_questions.all())
-    for a in teacher.assignments.all():
-        q_list.extend(list(a.questions.all().values_list("pk", flat=True)))
-    q_list = list(set(q_list))
-
-    convincing = Answer.objects.filter(
-        chosen_rationale__isnull=False
-    ).values_list("chosen_rationale", flat=True)
-
     no_score_needed = (
-        AnswerAnnotation.objects.annotate(times_scored=Count("answer"))
-        .filter(
-            Q(times_scored__gt=2)
-            | Q(annotator=teacher.user)
-            | Q(score__isnull=False)
-        )
+        AnswerAnnotation.objects.filter(score__isnull=False)
+        .annotate(times_scored=Count("answer"))
+        .filter(Q(times_scored__gt=2) | Q(annotator=teacher.user))
         .values_list("answer", flat=True)
     )
-
-    if convincing:
+    # print(no_score_needed.count(), " answers no score needed")
+    # print(no_score_needed)
+    #
+    if convincing_rationales.count() > 0:
         answers = (
-            Answer.objects.filter(
-                Q(question__discipline__in=teacher.disciplines.all())
-                | Q(question_id__in=q_list)
+            Answer.objects.filter(pk__in=convincing_rationales).exclude(
+                pk__in=no_score_needed
             )
-            .exclude(second_answer_choice__isnull=True)
-            .filter(pk__in=convincing)
-            .exclude(pk__in=no_score_needed)
-            .annotate(times_shown=Count("shown_answer"))
-            .order_by("-times_shown")
+            # .annotate(times_shown=Count("shown_answer"))
+            # .order_by("-times_shown")
         )
+        # print("****")
+        # print(answers.values("pk","times_shown"))
     else:
-        answers = (
-            Answer.objects.filter(
-                Q(question__discipline__in=teacher.disciplines.all())
-                | Q(question_id__in=q_list)
-            )
-            .exclude(second_answer_choice__isnull=True)
-            .exclude(first_answer_choice=F("second_answer_choice"))
-            .exclude(pk__in=no_score_needed)
-            .annotate(times_shown=Count("shown_answer"))
-            .order_by("-times_shown")
-        )
+        answers = convincing_rationales
 
-    if not answers:
-        return []
-    else:
-        return answers[:n]
+    # print("final set:")
+    # print(answers)
+
+    return answers[:n]
