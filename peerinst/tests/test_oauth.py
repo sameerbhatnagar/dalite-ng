ea7ed5d380ca54767b0ff5a55bc47af5b0107462
jsumnerPhD <jonathon.r.e.sumner@gmail.com>
2019-12-15 15:23:41 -0500

    Oauth test passes

--- peerinst/tests/test_oauth.py
+++ peerinst/tests/test_oauth.py
@@ -1,20 +1,61 @@
-import uuid
+import binascii
+import hmac
+import time
+
+from hashlib import sha1
 
 
 def test_oauth_signature(client, settings):
-    settings.LTI_CLIENT_KEY = str(uuid.uuid1())
-    settings.LTI_CLIENT_SECRET = str(uuid.uuid1())
+    """
+    See:
+    https://oauth1.wp-api.org/docs/basics/Signing.html
+    https://github.com/joestump/python-oauth2/blob/release/1.9/oauth2/__init__.py
+    """
 
-    response = client.post(
-        "/lti/",
-        {
-            "oauth_timestamp": uuid.uuid1().int,
-            "oauth_nonce": uuid.uuid1(),
-            "oauth_signature_method": "HMAC-SHA1",
-            "oauth_signature": str(uuid.uuid1()),
-        },
+    settings.LTI_CLIENT_KEY = "abcd"  # The consumer key
+    settings.LTI_CLIENT_SECRET = "1234"  # The consumer secret
+
+    oauth_parameters = {
+        "user_id": "test",
+        "custom_assignment_id": "test-assignment",
+        "custom_question_id": "1",
+        "oauth_consumer_key": settings.LTI_CLIENT_KEY,
+        "oauth_nonce": "nonce",
+        "oauth_signature_method": "HMAC-SHA1",
+        "oauth_timestamp": str(int(time.time())),
+    }
+
+    # Build oauth base string
+    # - Must be url encoded
+    # - Must start with uppercase http method
+    # - Must be followed by lowercase url, no port if 80 or 443
+    # - Must be followed by request parameters, alphabetical by keys,
+    #   excluding oauth_signature, concatenated with &
+    base_string = (
+        "POST&http%3A%2F%2Ftestserver%2Flti%2F&"
+        + "custom_assignment_id%3D"
+        + oauth_parameters["custom_assignment_id"]
+        + "%26custom_question_id%3D"
+        + oauth_parameters["custom_question_id"]
+        + "%26oauth_consumer_key%3D"
+        + oauth_parameters["oauth_consumer_key"]
+        + "%26oauth_nonce%3Dnonce%26oauth_signature_method%3DHMAC-SHA1%26oauth_timestamp%3D"  # noqa E501
+        + oauth_parameters["oauth_timestamp"]
+        + "%26user_id%3Dtest"
     )
+
+    # Build oauth signature from basestring and consumer secret (no token here)
+    key = settings.LTI_CLIENT_SECRET + "&"
+
+    hashed = hmac.new(key, base_string, sha1)
+    oauth_signature = binascii.b2a_base64(hashed.digest())[:-1]
+
+    oauth_parameters.update({"oauth_signature": oauth_signature})
+
+    response = client.post("/lti/", oauth_parameters, follow=True)
 
     print(response)
 
-    assert response.status == 200
+    assert (
+        response.status_code == 404
+    )  # Assignment does not exist, but oauth protocol succeeds
