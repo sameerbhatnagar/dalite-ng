b11e1905f065011bd916640008f82c63428c89b3
Antoine Lefebvre-Brossard <antoinelb@protonmail.com>
2020-01-06 15:28:40 -0500

    Changed url to re_path

--- tos/urls.py
+++ tos/urls.py
@@ -1,46 +1,42 @@
-from django.conf.urls import url
+from django.urls import re_path
 from . import views
 
 app_name = "tos"
 urlpatterns = [
-    url(r"^required/$", views.tos.tos_required, name="tos_required"),
-    url(
-        r"^tos/(?P<role>\w{1,})/(?P<version>\d{1,})/update/$",
+    re_path(r"^required/$", views.tos.tos_required, name="tos_required"),
+    re_path(
+        r"^tos/<role>/<int:version>/update/$",
         views.tos.tos_consent_update,
         name="tos_update",
     ),
-    url(
-        r"^tos/(?P<role>[a-z]{1,})/(?P<version>\d{1,})/modify/$",
+    re_path(
+        r"^tos/<role>/<int:version>/modify/$",
         views.tos.tos_consent_modify,
         name="tos_modify",
     ),
-    url(
-        r"^tos/(?P<role>[a-z]{1,})/modify/$",
+    re_path(
+        r"^tos/<role>/modify/$",
         views.tos.tos_consent_modify,
         name="tos_modify",
     ),
-    url(
-        r"^tos/(?P<role>[a-z]{1,})/(?P<version>\d{1,})/$",
+    re_path(
+        r"^tos/<role>/(?P<version>\d{1,})/$",
         views.tos.tos_consent,
         name="tos_consent",
     ),
-    url(
-        r"^tos/(?P<role>[a-z]{1,})/$",
-        views.tos.tos_consent,
-        name="tos_consent",
-    ),
-    url(
-        r"^email/(?P<role>[a-z]{1,})/modify$",
+    re_path(r"^tos/<role>/$", views.tos.tos_consent, name="tos_consent",),
+    re_path(
+        r"^email/<role>/modify$",
         views.email.email_consent_modify,
         name="email_modify",
     ),
-    url(
-        r"^email/(?P<role>[a-z]{1,})/update$",
+    re_path(
+        r"^email/<role>/update$",
         views.email.email_consent_update,
         name="email_update",
     ),
-    url(
-        r"^email/(?P<role>[a-z]{1,})/change$",
+    re_path(
+        r"^email/<role>/change$",
         views.email.change_user_email,
         name="email_change",
     ),
