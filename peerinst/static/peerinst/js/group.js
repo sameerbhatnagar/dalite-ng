4c431212b259badfc10af3e28f3b86fb2a4aa0fd
Arthur Ayestas Hilgert <aahilgert@gmail.com>
2020-02-21 18:27:03 -0500

    added linking front end dialog to teacher detail view -- need to fix js bug

--- peerinst/static/peerinst/js/group.js
+++ peerinst/static/peerinst/js/group.js
@@ -1,4 +1,5 @@
 export { editField, saveField } from "./_group/common.js";
-export { createCollection, initCourseLinking } from "./_group/details.js";
+export { createCollection } from "./_group/details.js";
 export { initAssignment } from "./_group/assignment.js";
 export { init as initDetails } from "./_group/details";
+export { getSnack } from "./_group/common.js";
--- peerinst/views/group.py
+++ peerinst/views/group.py
@@ -18,6 +18,7 @@ from peerinst.models import (
     StudentGroup,
     StudentGroupAssignment,
     Teacher,
+    StudentGroupCourse,
 )
 
 from .decorators import group_access_required
@@ -102,8 +103,18 @@ def group_details_page(req, group_hash, teacher, group):
             for c in student_reputation_criteria
         ],
         "owned_courses": get_owned_courses(teacher.user),
+        "connected_course": StudentGroupCourse.objects.filter(
+            student_group=group
+        ).first(),
     }
-
+    context["is_connected_to_course"] = False
+    if StudentGroupCourse.objects.filter(student_group=group).first():
+        context["is_connected_to_course"] = True
+        context["connected_course"] = (
+            StudentGroupCourse.objects.filter(student_group=group)
+            .first()
+            .course
+        )
     return render(req, "peerinst/group/details.html", context)
 
 
