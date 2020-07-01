eb607ca1bf57a8e9c31a965f9fcb048dd09e0bc3
jsumnerPhD <jonathon.r.e.sumner@gmail.com>
2020-07-01 14:54:37 -0400

    Refactor showMeMore template js to file; fix linting errors.

--- peerinst/static/peerinst/js/_question/review.js
+++ peerinst/static/peerinst/js/_question/review.js
@@ -45,6 +45,37 @@ function submitButtonView() {
   }
 }
 
+function showMeMore() {
+  [].forEach.call(document.querySelectorAll(".expand-button"), function (
+    el,
+    i,
+  ) {
+    el.addEventListener("click", function () {
+      const els = document.getElementsByClassName(
+        "hidden-" + el.getAttribute("data-rationale-iterator"),
+      );
+      const showCounter = document.getElementById(
+        "show-counter-" + el.getAttribute("data-rationale-iterator"),
+      );
+      let shownCounter = 0;
+      for (let i = 0; i < els.length; i++) {
+        if (els[i].hidden == true && shownCounter < 2) {
+          els[i].hidden = false;
+          shownCounter++;
+          if (i == els.length - 1) {
+            el.hidden = true;
+            break;
+          }
+          showCounter.setAttribute(
+            "value",
+            +showCounter.getAttribute("value") + 1,
+          );
+        }
+      }
+    });
+  });
+}
+
 /********/
 /* init */
 /********/
@@ -53,4 +84,5 @@ export function init() {
   initModel();
   view();
   startSubmitAllowedTimer(5);
+  showMeMore();
 }
