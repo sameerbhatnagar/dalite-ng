c379950c6bb29ea44a1cc2d8cfbd6ed81ad6fcfd
jsumnerPhD <jonathon.r.e.sumner@gmail.com>
2020-08-05 11:26:50 -0400

    Remove old volumes!

--- peerinst/static/peerinst/js/_assignment/question.js
+++ peerinst/static/peerinst/js/_assignment/question.js
@@ -19,11 +19,11 @@ import { Typography } from "@rmwc/typography";
 
 import { colours, PlotConfusionMatrix } from "./analytics.js";
 
+import "@rmwc/icon-button/node_modules/@material/icon-button/dist/mdc.icon-button.min.css";
 import "@rmwc/card/node_modules/@material/card/dist/mdc.card.min.css";
 import "@rmwc/typography/node_modules/@material/typography/dist/mdc.typography.min.css";
 import "@rmwc/dialog/node_modules/@material/dialog/dist/mdc.dialog.min.css";
 import "@rmwc/button/node_modules/@material/button/dist/mdc.button.min.css";
-import "@rmwc/icon-button/node_modules/@material/icon-button/dist/mdc.icon-button.min.css";
 import "@rmwc/theme/node_modules/@material/theme/dist/mdc.theme.min.css";
 import "@rmwc/icon/icon.css";
 
